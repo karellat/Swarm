652caa04f8d39eec23848688d8686f697de0544b
Tomas Karella <tomas.karella@gmail.com>
2017-05-05 19:34:07 +0200

    IRobot interface created

--- SwarmSimFramework/SwarmSimFramework/Interfaces/IRobotBrain.cs
+++ SwarmSimFramework/SwarmSimFramework/Interfaces/IRobotBrain.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace SwarmSimFramework.Interfaces
 {
-    class IRobotBrain
+    public interface  IRobotBrain
     {
+        /// <summary>
+        /// Decide based on sensor read values
+        /// </summary>
+        /// <param name="readValues"></param>
+        /// <returns></returns>
+       float[] Decide(float[] readValues);
+        /// <summary>
+        /// Minimum possible value of forwarding values
+        /// </summary>
+        float MinOutputValue { get; }
+        /// <summary>
+        /// Maximum possible value of forwarding values
+        /// </summary>
+        float MaxOutputValue { get;  }
+        /// <summary>
+        /// Minimum possible value of receiving values 
+        /// </summary>
+        float MinInputValue { get;  }
+        /// <summary>
+        /// Maximum possible value of receiving values 
+        /// </summary>
+        float MaxInputValue { get;  }
+        /// <summary>
+        /// Dimension of recieving values
+        /// </summary>
+        int InputDimension { get;  }
+        /// <summary>
+        /// Dimension of forwarding values
+        /// </summary>
+        int OutputDimension { get;  }
+
+        IRobotBrain GetCleanCopy();
     }
 }
--- diary.md
+++ diary.md
@@ -1,7 +1,18 @@
 # Diary of project
 # FrameWork for: Evolutionary Algorithms for the Control of Heterogeneous Robotic Swarm
 
+## TODO: 
+
+## Q&A: 
+### Implementation:  
+* How long should be GetString();
+### Evolution: 
+* 
+
+
 * 05.05.2017 - 
 	* refactoring old solutions
 	* object design(IEntity was not created due to many abstract classes, no way to avoid virtual call)
 	* sensor -> child of entity & inherit ISensor interface
+	* use more asserts
+	* brain serializer/deserializer 
