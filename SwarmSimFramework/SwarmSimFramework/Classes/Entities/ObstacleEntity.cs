05835c2dc481418a0ea12326fc1cec5b503c5715
Tomas Karella <tomas.karella@gmail.com>
2017-05-18 21:35:10 +0200

    ObstacleEntity added

---
+++ SwarmSimFramework/SwarmSimFramework/Classes/Entities/ObstacleEntity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwarmSimFramework.Classes.Entities
+{
+    /// <summary>
+    /// Represent Circle obstacle 
+    /// </summary>
+    class ObstacleEntity : CircleEntity
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="middle"></param>
+        /// <param name="radius"></param>
+        public ObstacleEntity(Vector2 middle,float radius) : base(middle,radius,"Obstacle Entity")
+        {
+            Color = EntityColor.ObstacleColor;
+            
+        }
+        /// <summary>
+        /// Create a clone of entity
+        /// </summary>
+        /// <returns></returns>
+        public override Entity DeepClone()
+        {
+            return (Entity) this.MemberwiseClone();
+            
+        }
+    }
+}
