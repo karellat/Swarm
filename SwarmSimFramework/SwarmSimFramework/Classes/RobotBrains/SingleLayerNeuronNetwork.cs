88847e2c2b22174be6aaffaf78da8c1f198882aa
Tomas Karella <tomas.karella@gmail.com>
2017-05-20 17:55:51 +0200

    Single layered network created

---
+++ SwarmSimFramework/SwarmSimFramework/Classes/RobotBrains/SingleLayerNeuronNetwork.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+using SwarmSimFramework.Classes.Entities;
+using SwarmSimFramework.Interfaces;
+using SwarmSimFramework.SupportClasses;
+
+namespace SwarmSimFramework.Classes.RobotBrains
+{
+    public class SingleLayerNeuronNetwork : IRobotBrain
+    {
+        /// <summary>
+        /// expected values of weights 
+        /// </summary>
+        public static float ExpectedValue;
+        /// <summary>
+        /// variance of weights 
+        /// </summary>
+        public static float Variance;
+        /// <summary>
+        /// Variance of adding values of mutation 
+        /// </summary>
+        public static float VarianceOfGaussianMutations;
+        /// <summary>
+        /// Permille propability of mutation
+        /// </summary>
+        public static int PermilleOfMutation;
+        /// <summary>
+        /// Activation funcs 
+        /// </summary>
+        protected static Func<float, float> ActivationFunc; 
+
+        /// <summary>
+        /// Fitness of current brain
+        /// </summary>
+        public double Fitness { get; set; }
+        /// <summary>
+        /// Transformation fncs for output
+        /// </summary>
+        public Func<float, float> ActivationFnc => SingleLayerNeuronNetwork.ActivationFunc;
+
+        /// <summary>
+        /// Input & Output dimensions
+        /// </summary>
+        public IODimension IoDimension { get; protected set; }
+        /// <summary>
+        /// bounds for input & outpus
+        /// </summary>
+        public Bounds InOutBounds { get; protected set; }
+        /// <summary>
+        /// Intern neurons of single output 
+        /// </summary>
+        public Perceptron[] Neurons { get; protected set; }
+        /// <summary>
+        /// Make all neurons decide, set input
+        /// </summary>
+        /// <param name="readValues"></param>
+        /// <returns></returns>
+        public float[] Decide(float[] readValues)
+        {
+           float[] o = new float[IoDimension.Output];
+            for (int i = 0; i < IoDimension.Output; i++)
+            {
+                o[i] = Neurons[i].Decide(readValues);
+            }
+
+            return o;
+        }
+        /// <summary>
+        /// Generate clean copy of brain
+        /// </summary>
+        /// <returns></returns>
+        public IRobotBrain GetCleanCopy()
+        {
+            var nn = (SingleLayerNeuronNetwork) this.MemberwiseClone();
+            for (int i = 0; i < this.IoDimension.Output; i++)
+            {
+                Neurons[i] = (Perceptron) Neurons[i].GetCleanCopy();
+            }
+
+            return nn;
+
+        }
+        /// <summary>
+        /// Mutate every percepton 
+        /// </summary>
+        public void Mutate()
+        {
+            foreach (var n in Neurons)
+            {
+                n.Mutate(PermilleOfMutation,VarianceOfGaussianMutations);
+            }  
+        }
+        /// <summary>
+        /// Generate random single layered network with given static values
+        /// </summary>
+        /// <param name="dimension"></param>
+        /// <returns></returns>
+        public static SingleLayerNeuronNetwork GenerateNewRandomNetwork(IODimension dimension)
+        {
+            var nn = new SingleLayerNeuronNetwork
+            {
+                IoDimension = dimension,
+                InOutBounds = RobotEntity.StandardBounds
+            };
+            nn.Neurons = new Perceptron[nn.IoDimension.Output];
+            for (int i = 0; i < nn.IoDimension.Output; i++)
+            {
+                nn.Neurons[i] = Perceptron.GenerateRandom(ExpectedValue,Variance,nn.ActivationFnc,nn.IoDimension);
+            }
+
+            return nn;
+        }
+        /// <summary>
+        /// Return new mutated brain
+        /// </summary>
+        /// <returns></returns>
+        public SingleLayerNeuronNetwork CreateMutatedNetwork()
+        {
+            var nn = (SingleLayerNeuronNetwork) this.GetCleanCopy();
+            nn.Mutate();
+            return nn;
+        }
+    }
+}
