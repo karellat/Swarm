7669908f2a01f5b6694702e6c9b1a72f435395aa
Tomas Karella <tomas.karella@gmail.com>
2017-05-21 09:52:52 +0200

    First scenario robot completed

--- SwarmSimFramework/SwarmSimFramework/Classes/Robots/ScoutRobot.cs
+++ SwarmSimFramework/SwarmSimFramework/Classes/Robots/ScoutRobot.cs
@@ -4,38 +4,41 @@ using SwarmSimFramework.Classes.Entities;
 
 namespace SwarmSimFramework.Classes.Robots
 {
+    /// <summary>
+    /// ScoutRobot for the 1. Scenario
+    /// </summary>
     public class ScoutRobot : RobotEntity
     {
-        public ScoutRobot(Vector2 middle, float radius, float amountOfFuel,float orientation = 0 )
-            : base(middle, radius, "ScoutRobot",null ,null , amountOfFuel, 0, 1, 100, 100, -100, orientation)
+        public ScoutRobot(Vector2 middle, float amountOfFuel,float orientation = 0 )
+            : base(middle, 2.5f, "ScoutRobot",null ,null , amountOfFuel, 0, 1, 100, 100, -100, orientation)
         {
-            ISensor[] newSensors = new ISensor[12];
+            ISensor[] sensors = new ISensor[12];
             //FUEL Sensors
-            newSensors[0] = new FuelLineSensor(this,10,DegreesToRadians(45));
-            newSensors[1] = new FuelLineSensor(this,10,0);
-            newSensors[2] = new FuelLineSensor(this,10,DegreesToRadians(-45));
+            sensors[0] = new FuelLineSensor(this,10,DegreesToRadians(45));
+            sensors[1] = new FuelLineSensor(this,10,0);
+            sensors[2] = new FuelLineSensor(this,10,DegreesToRadians(-45));
             //Line Type Sensors
-            newSensors[3] = new LineTypeSensor(this,10,DegreesToRadians(45));
-            newSensors[4] = new LineTypeSensor(this,10,0);
-            newSensors[5] = new LineTypeSensor(this,10,DegreesToRadians(-45));
+            sensors[3] = new LineTypeSensor(this,10,DegreesToRadians(45));
+            sensors[4] = new LineTypeSensor(this,10,0);
+            sensors[5] = new LineTypeSensor(this,10,DegreesToRadians(-45));
             //Locator
-            newSensors[6] = new LocatorSensor(this);
+            sensors[6] = new LocatorSensor(this);
             //Type Line sensor
-            newSensors[7] =  new TypeCircleSensor(this,50);
+            sensors[7] =  new TypeCircleSensor(this,50);
             //Touch sensors
-            newSensors[8] = new TouchSensor(this,0.1f,DegreesToRadians(90));
-            newSensors[9] = new TouchSensor(this, 0.1f, DegreesToRadians(180));
-            newSensors[10] = new TouchSensor(this,0.1f,DegreesToRadians(270));
+            sensors[8] = new TouchSensor(this,0.1f,DegreesToRadians(90));
+            sensors[9] = new TouchSensor(this, 0.1f, DegreesToRadians(180));
+            sensors[10] = new TouchSensor(this,0.1f,DegreesToRadians(270));
             //Radio sensor 
-            newSensors[11] = new RadioSensor(this,100);
+            sensors[11] = new RadioSensor(this,100);
 
-            this.Sensors = newSensors;
+            this.Sensors = sensors;
 
-            IEffector[] newEffectors = new IEffector[2];
-            newEffectors[0] = new TwoWheelMotor(this,3);
-            newEffectors[1] = new RadioTransmitter(this,200);
+            IEffector[] effectors = new IEffector[2];
+            effectors[0] = new TwoWheelMotor(this,3);
+            effectors[1] = new RadioTransmitter(this,200);
 
-            this.Effectors = newEffectors;
+            this.Effectors = effectors;
 
             this.CountDimension();
         }
--- SwarmSimFramework/SwarmSimFramework/Classes/Robots/WorkerRobot.cs
+++ SwarmSimFramework/SwarmSimFramework/Classes/Robots/WorkerRobot.cs
@@ -1,4 +1,5 @@
 ﻿using System.Numerics;
+using SwarmSimFramework.Classes.Effectors;
 using SwarmSimFramework.Classes.Entities;
 
 namespace SwarmSimFramework.Classes.Robots
@@ -9,8 +10,38 @@ namespace SwarmSimFramework.Classes.Robots
     public class WorkerRobot:RobotEntity
     {
         public WorkerRobot(Vector2 middle, float radius,float amountOfFuel,float orientation = 0) 
-            : base(middle, radius, "WorkerRobot", null, null, amountOfFuel, 5, 1, 100, 100, -100, orientation)
+            : base(middle, 5, "WorkerRobot", null, null, amountOfFuel, 5, 1, 100, 100, -100, orientation)
         {
+            ISensor[] sensors = new ISensor[11];
+            //FUEL Sensors
+            sensors[0] = new FuelLineSensor(this, 10, DegreesToRadians(45));
+            sensors[1] = new FuelLineSensor(this, 10, 0);
+            sensors[2] = new FuelLineSensor(this, 10, DegreesToRadians(-45));
+            //Line Type Sensors
+            sensors[3] = new LineTypeSensor(this, 10, DegreesToRadians(45));
+            sensors[4] = new LineTypeSensor(this, 10, 0);
+            sensors[5] = new LineTypeSensor(this, 10, DegreesToRadians(-45));
+            //Locator
+            sensors[6] = new LocatorSensor(this);
+            //Touch sensors
+            sensors[7] = new TouchSensor(this, 0.1f, DegreesToRadians(90));
+            sensors[8] = new TouchSensor(this, 0.1f, DegreesToRadians(180));
+            sensors[9] = new TouchSensor(this, 0.1f, DegreesToRadians(270));
+            //Radio sensor 
+            sensors[10] = new RadioSensor(this, 100);
+
+            this.Sensors = sensors;
+
+            IEffector[] effectors = new IEffector[3];
+            effectors[0] = new TwoWheelMotor(this, 1.5f);
+            effectors[1] = new RadioTransmitter(this, 200);
+            effectors[2] = new Picker(this,15,0);
+
+            this.Effectors = effectors;
+
+            this.CountDimension();
+
+
         }
     }
 }
